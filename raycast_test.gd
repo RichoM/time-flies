b978a3a2b1471c15f333668ca9025936546081c0
RichoM <richi.moran@gmail.com>
2022-05-17 11:34:25 -0300

    Removed unused assets

--- raycast_test.gd
+++
@@ -1,9 +0,0 @@
-extends Node2D
-
-onready var ray = $ray
-onready var line = $line
-
-func _process(delta):
-	var mouse_pos = get_local_mouse_position()
-	line.points[1] = mouse_pos
-	ray.cast_to = mouse_pos
