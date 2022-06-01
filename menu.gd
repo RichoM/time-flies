6dada17f15e46dc78b6c9663caa2c1fba256e128
RichoM <richi.moran@gmail.com>
2022-06-01 15:28:37 -0300

    Made the clock in the title screen oscillate

--- menu.gd
+++ menu.gd
@@ -5,6 +5,7 @@ onready var begin_time = OS.get_ticks_msec()
 func _process(delta_):
 	var t = (OS.get_ticks_msec() - begin_time) / 1000.0
 	$PlayButton.rotation_degrees = sin(t*2) * 2.5
+	$Clock.rotation_degrees = cos(t*3) * 15
 	
 func _on_play_button_pressed():
 	get_tree().change_scene("res://Level.tscn")
